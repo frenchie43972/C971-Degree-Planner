31015b81ddaf3c3dc490be7fd02d8c1778a1888b
frenchie43972 <frenchie43972@gmail.com>
2022-10-21 08:38:40 +0900

    Working Courses

---
+++ DegreePlanner/DegreePlanner/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace DegreePlanner.Models
+{
+	public class Course
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public int TermId { get; set; }
+		public string CourseName { get; set; }
+		public string CourseStatus { get; set; }
+		public DateTime CourseStart { get; set; }
+		public DateTime CourseEnd { get; set; }
+		public string InstName { get; set; }
+		public string InstEmail { get; set; }
+		public string InstPhone { get; set; }
+		public string Notes { get; set; }
+		public bool Notification { get; set; }
+	}
+}
